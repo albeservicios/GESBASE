3f9924f9a23578d23514b2746f77261989e3033c
ALBE SERVICIOS GENERALES <julianbattauz006@gmail.com>
2026-08-12 21:03:30 -0300

    Initialize Firebase and export services

---
+++ firebase.js
@@ -0,0 +1,36 @@
+// ==========================================
+// GESBASE - CONEXIÓN FIREBASE
+// ==========================================
+
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { getStorage } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Servicios
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+
+// Exportar
+export {
+    app,
+    auth,
+    db,
+    storage
+};
