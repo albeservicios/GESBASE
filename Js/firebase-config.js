be50ad7a22615b97d64a0147006ca8f97adb2cd7
ALBE SERVICIOS GENERALES <julianbattauz006@gmail.com>
2026-08-26 07:39:15 -0300

    Update Firebase configuration API key

--- Js/firebase-config.js
+++ Js/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "TU_API_KEY_NUEVA",
-    authDomain: "gesbase-4bf94.firebaseapp.com",
-    projectId: "gesbase-4bf94",
-    storageBucket: "gesbase-4bf94.firebasestorage.app",
-    messagingSenderId: "64959778874",
-    appId: "1:64959778874:web:c3bc129c3cee8cb77d6503",
-    measurementId: "G-CCKGSRVGEX"
+  apiKey: "AIzaSyDDCQ7WhS9Gy8DwQGZ01VaFTmSgV2rjs9o",
+  authDomain: "gesbase-4bf94.firebaseapp.com",
+  projectId: "gesbase-4bf94",
+  storageBucket: "gesbase-4bf94.firebasestorage.app",
+  messagingSenderId: "64959778874",
+  appId: "1:64959778874:web:c3bc129c3cee8cb77d6503",
+  measurementId: "G-CCKGSRVGEX"
 };
