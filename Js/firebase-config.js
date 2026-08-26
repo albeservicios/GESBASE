9df2170cdf876eadcf4830fda5f1b0727b9b8a7e
ALBE SERVICIOS GENERALES <julianbattauz006@gmail.com>
2026-08-25 23:33:45 -0300

    Fix property names in firebase config
    Updated the firebase configuration object to correct property names.

--- Js/firebase-config.js
+++ Js/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDDCQ7WhS9Gy8DwGQZ01VaFTmSgV2rjs9o",
-    authDomain: "gesbase-4bf94.firebaseapp.com",
-    projectId: "gesbase-4bf94",
-    storageBucket: "gesbase-4bf94.firebasestorage.app",
-    messagingSenderId: "64959778874",
-    appId: "1:64959778874:web:c3bc129c3cee8cb77d6503",
-    measurementId: "G-CCKGSRVGEX"
+apiKey: "AIzaSyDDCQ7WhS9Gy8DwQGZ01VaFTmSgV2rjs9o",
+authDomain: "gesbase-4bf94.firebaseapp.com",
+ID del proyecto: "gesbase-4bf94",
+StorageBucket: "gesbase-4bf94.firebasestorage.app",
+messagingSenderId: "64959778874",
+appId: "1:64959778874:web:c3bc129c3cee8cb77d6503",
+measurementId: "G-CCKGSRVGEX"
 };
