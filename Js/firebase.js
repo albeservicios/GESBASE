d3493a3e80c7f0343c08a4cf3bd661ae84b27e75
ALBE SERVICIOS GENERALES <julianbattauz006@gmail.com>
2026-08-25 23:34:46 -0300

    Refactor firebase.js for improved readability

--- Js/firebase.js
+++ Js/firebase.js
@@ -1,1 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";  import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";  import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";  import { firebaseConfig } from "./firebase-config.js";  const app = initializeApp(firebaseConfig);  const auth = getAuth(app);  const db = getFirestore(app);  const almacenamiento = getStorage(aplicación);  exportar { aplicación, autoridad, base de datos, almacenamiento };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const almacenamiento = getStorage(app);
+
+export {
+    app,
+    auth,
+    db,
+    almacenamiento
+};
