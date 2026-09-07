aa283c0fa6ffb86c0a87f3fb876c786b2ee849b2
ALBE SERVICIOS GENERALES <julianbattauz006@gmail.com>
2026-09-07 14:19:33 -0300

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "gesbase-red-v1";
+
+const ARCHIVOS = [
+  "./",
+  "./index.html",
+  "./red.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARCHIVOS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
